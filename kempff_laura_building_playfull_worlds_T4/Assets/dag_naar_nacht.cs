ca1968ce27f5309c1c1821c5ebb772ddf60c47be
Laura <lauraskempff@gmail.com>
2020-05-28 11:06:25 +0200

    Nacht verlichting + trigger die dag naar nacht veranderd

---
+++ kempff_laura_building_playfull_worlds_T4/Assets/dag_naar_nacht.cs
@@ -0,0 +1,28 @@
+﻿
+using UnityEngine;
+
+public class dag_naar_nacht : MonoBehaviour
+{
+    
+    public GameObject light;
+    public GameObject night_light;
+
+    public Material nacht;
+
+    private bool on = true;
+
+    void OnTriggerStay(Collider plyr) 
+    {
+        if (plyr.tag == "Player") ;
+        {
+            light.SetActive(false);
+            on = false;
+
+            night_light.SetActive(true);
+            on = true;
+
+            RenderSettings.skybox = nacht;
+        }
+
+    }
+}
